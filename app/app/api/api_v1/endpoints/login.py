017301dcf2a3b29f3ca2d8156afe65e0286ec90e
Pythonista WangXB <739221432@qq.com>
2020-04-30 20:57:56 +0800

    '增加了注册页面与查看所有用户、根据email查询用户的操作'

--- app/app/api/api_v1/endpoints/login.py
+++ app/app/api/api_v1/endpoints/login.py
@@ -83,15 +83,15 @@ class Item(BaseModel):
     answer: str
 
 
-@router.post('/code/confirm/', response_model=ReturnItems)
-async def dec(item: Item):
-    if red.exists(item.uid):
-        true = red.get(item.uid).decode('ascii')
-        if true == item.answer:
-            return {'status': '200', 'info': '验证码正确'}
-        return {'status': '403', 'info': '验证码错误'}
-    else:
-        return {'status': '404', 'info': '验证码不存在'}
+# @router.post('/code/confirm/', response_model=ReturnItems)
+# async def dec(item: Item):
+#     if red.exists(item.uid):
+#         true = red.get(item.uid).decode('ascii')
+#         if true == item.answer:
+#             return {'status': '200', 'info': '验证码正确'}
+#         return {'status': '403', 'info': '验证码错误'}
+#     else:
+#         return {'status': '404', 'info': '验证码不存在'}
 
 
 @router.post("/login/access-token", response_model=schemas.Token)
--- app/app/crud/base.py
+++ app/app/crud/base.py
@@ -4,7 +4,9 @@ from fastapi.encoders import jsonable_encoder
 from pydantic import BaseModel
 from sqlalchemy.orm import Session
 import os
-os.sys.path.append(r"C:\Users\Administrator\Desktop\full-stack-fastapi-postgresql\{{cookiecutter.project_slug}}\backend\app")
+
+os.sys.path.append(
+    r"C:\Users\Administrator\Desktop\full-stack-fastapi-postgresql\{{cookiecutter.project_slug}}\backend\app")
 
 from app.db.base_class import Base
 
@@ -29,9 +31,9 @@ class CRUDBase(Generic[ModelType, CreateSchemaType, UpdateSchemaType]):
         return db.query(self.model).filter(self.model.id == id).first()
 
     def get_multi(
-        self, db: Session, *, skip: int = 0, limit: int = 100
+            self, db: Session, *, skip: int = 0, limit: int = 10000
     ) -> List[ModelType]:
-        return db.query(self.model).offset(skip).limit(limit).all()
+        return db.query(self.model).filter(self.model.is_superuser == 0).offset(skip).limit(limit).all()
 
     def create(self, db: Session, *, obj_in: CreateSchemaType) -> ModelType:
         obj_in_data = jsonable_encoder(obj_in)
@@ -42,11 +44,11 @@ class CRUDBase(Generic[ModelType, CreateSchemaType, UpdateSchemaType]):
         return db_obj
 
     def update(
-        self,
-        db: Session,
-        *,
-        db_obj: ModelType,
-        obj_in: Union[UpdateSchemaType, Dict[str, Any]]
+            self,
+            db: Session,
+            *,
+            db_obj: ModelType,
+            obj_in: Union[UpdateSchemaType, Dict[str, Any]]
     ) -> ModelType:
         obj_data = jsonable_encoder(db_obj)
         if isinstance(obj_in, dict):
